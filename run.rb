8db3002b383893307c69e95e68d7418df3f87c5f
Yamanaka Hiroyuki <lancers333-01@yamanakahiroyuki-no-MacBook-Pro.local>
2017-03-09 11:55:36 +0900

    first commit

---
+++ run.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+def ip
+  "2#{rand(9)}#{rand(9)}.0.1#{rand(9)}#{rand(9)}.1#{rand(9)}#{rand(9)}"
+end
+
+def identd
+  '-'
+end
+
+def user_id
+  '-'
+end
+
+def time
+  Time.now.strftime('[%d/%B/%Y:%H:%M:%S -0700]')
+end
+
+def path
+  '"GET ' +
+  (
+    rand(2) == 0 ? '/' : "/ranking/?page=#{rand(3)}"
+  ) +
+  ' HTTP/1.0"'
+end
+
+def status
+  200
+end
+
+def size
+  rand(3000) + 5
+end
+
+loop do
+  puts [ip,identd,time,path,status,size].join(" ")
+end
